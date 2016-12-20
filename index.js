ec8139633603060649a445523dc87f1cf45d03a2
Chavi Malhotra <Chavi@Chavis-MacBook-Pro.local>
2016-12-20 11:34:41 -0800

    format socket stream as per configuration

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var mongoose = require('mongoose');
+var julian = require('julian');
+var async = require('async');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -33,9 +35,6 @@ mongoose.connection.on('disconnected', function () {
   console.log('Mongoose default connection disconnected');
 });
 
-//Load the model for SatData1
-var satData1 = require('./app/model/satData1');
-
 app.use('/public', express.static('public'));
 
 app.get('/', function(request, response) {
@@ -43,35 +42,5 @@ app.get('/', function(request, response) {
 	response.end();
 });
 
-//Listen to the GMAT server streaming data
-io.on('connection', function(socket){
-	console.log('socket.io server connected.');
-
-	socket.on('satData1', function(data){
-		try {
-		    var parsedData = JSON.parse(data);
-		} catch (e) {
-		    console.log("not JSON");
-		}
-
-		var newSatData1 = new satData1({
-			timestamp : parsedData.timestamp, 
-			vehicleId : parsedData.vehicleId, 
-			vx: parsedData.vx, 
-			vy: parsedData.vy, 
-			vz: parsedData.vz, 
-			x: parsedData.x, 
-			y: parsedData.y,
-			z: parsedData.z
-		});
-
-		newSatData1.save(function(err) {
-		  if (err) throw err;
-
-		  console.log('Data saved successfully!');
-		});
-	});
-
-});
-
 require('./app/scripts/parseExcel.js')();
+require('./app/scripts/socket.js')(io, julian, async);
