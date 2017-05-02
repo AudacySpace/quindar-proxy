3f57ddba567cb6fc646eebeac7400f2896a8fb31
tarunigattu1992 <taruni.gattu@audacy.space>
2017-05-02 15:52:01 -0700

    updated query to save proxy application status

--- server/scripts/proxyAppStatus.js
+++ server/scripts/proxyAppStatus.js
@@ -1,8 +1,16 @@
+
 //Proxy application reporting proxy status to the database as unix timestamp
 var mongoose = require('mongoose');
 module.exports = function() {
+	var ProxyStatus = require('../model/proxystatus');
+	var appStatus = new ProxyStatus();
 	timer = setInterval(function(){
 		var unix = Math.round(+new Date()/1000);
-		mongoose.connection.collection('proxystatus').insert({proxyapptimestamp:unix});
-	}, 1000);
+		appStatus.timestamp =  unix;
+		appStatus.save(function(err){
+			if(err){
+				console.log(err);
+			}
+		});
+	  }, 1000);
 }
