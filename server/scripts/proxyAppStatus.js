96f9b8beee94e304181a2f9507f0d6b7bc0112ee
Chavi Malhotra <chavi.malhotra@gmail.com>
2017-07-13 13:12:28 -0700

    updated proxy status script

--- server/scripts/proxyAppStatus.js
+++ server/scripts/proxyAppStatus.js
@@ -1,16 +1,37 @@
-
 //Proxy application reporting proxy status to the database as unix timestamp
-var mongoose = require('mongoose');
 module.exports = function() {
 	var ProxyStatus = require('../model/proxystatus');
-	var appStatus = new ProxyStatus();
-	timer = setInterval(function(){
-		var unix = Math.round(+new Date()/1000);
-		appStatus.proxytimestamp =  unix;
-		appStatus.save(function(err){
-			if(err){
-				console.log(err);
-			}
-		});
-	  }, 1000);
+
+	ProxyStatus.findOne({}, function(err, status) {
+		if (err)
+			console.log("Error finding proxy status in DB: " + err);
+
+        if (status) {
+        	//update time if document exists
+        	timer = setInterval(function(){
+        		var timestamp = Math.round(+new Date()/1000);
+        		status.proxytimestamp =  timestamp;
+
+				status.save(function(err){
+					if(err){
+						console.log(err);
+					}
+				});
+			}, 1000);
+        }
+        else {
+        	//create a new document if not document exists
+        	var newStatus = new ProxyStatus();
+        	timer = setInterval(function(){
+        		var timestamp = Math.round(+new Date()/1000);
+        		newStatus.proxytimestamp =  timestamp;
+
+				newStatus.save(function(err){
+					if(err){
+						console.log(err);
+					}
+				});
+			}, 1000);
+        }
+    });
 }
