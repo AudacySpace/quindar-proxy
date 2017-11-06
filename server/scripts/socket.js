aa070d29a7aaff6ed1d9397f90ccb54f349e2905
Chavi Malhotra <chavi.malhotra@gmail.com>
2017-11-06 12:05:09 -0800

    added capability to send commands to simulator/spacecraft through socket

--- server/scripts/socket.js
+++ server/scripts/socket.js
@@ -3,12 +3,61 @@ module.exports = function(io, julian, async) {
 	var math = require('mathjs');
 	var Config = require('../model/configuration');
 	var Telemetry = require('../model/telemetry');
+	var Command = require('../model/command');
 	var source = "";
 	var configuration = new Config();
+	var newcommand = {};
+	var clients = {};
 
 	//Listen to the GMAT server streaming data
 	io.on('connection', function(socket){
 		console.log('socket.io server connected.');
+
+		//add clients socket id using mission name
+		socket.on('add-mission', function(data){
+			clients[data.mission] = {
+				"socket" : socket.id
+			}
+		})
+
+		socket.on('disconnect', function(){
+			for(var client in clients){
+		        if(clients[client]["socket"] == socket.id){
+					delete clients[client];
+		        }
+		    }
+		})
+
+		//poll database for new commands and send them to satellite/simulator
+		setInterval(function() {
+			Command.find( {'sent_to_satellite': false}, function(err, commands) {
+	            if(err){
+	                console.log(err);
+	            }
+
+	            if(commands.length>0) {
+					for(var i=0; i<commands.length; i++){
+						newcommand.name = commands[i].name;
+						newcommand.argument = commands[i].argument;
+						newcommand.timestamp = julian(commands[i].timestamp);
+
+						var room = clients[commands[i].mission]["socket"];
+						if(room) {
+							io.to(room).emit("command", newcommand);
+							commands[i].sent_to_satellite = true;
+
+							commands[i].save(function(err,result){
+								if(err){
+									console.log(err);
+								}
+
+								console.log("Flag updated for sent command");
+							})
+						}
+		            }
+	            }
+	        });
+		}, 1000);
 
 		socket.on('satData1', function(data){
 			source = socket.handshake.headers['x-real-ip'];
