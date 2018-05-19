7d044c3bbf8f0b59093c41d7842cae636a5673ca
tarunigattu1992 <taruni.gattu@audacy.space>
2018-05-18 17:12:43 -0700

    commiting backend source code to upload an aggregator csv file

--- app/controllers/sourceCtrl.js
+++ app/controllers/sourceCtrl.js
@@ -48,7 +48,7 @@ sourceApp
         });
     }
 
-    vm.showModal = function(missionName) {
+    vm.showModal = function(missionName,sourceIP) {
         // Just provide a template url, a controller and call 'open'.
         $uibModal.open({
             templateUrl: "./views/uploadAttachments.html",
@@ -57,6 +57,9 @@ sourceApp
             resolve: {
                 mission: function () {
                     return missionName;
+                },
+                sourceip: function(){
+                    return sourceIP;
                 }
             }
         }).result.then(function(response){
@@ -68,7 +71,7 @@ sourceApp
     };
 }]);
 
-sourceApp.controller('aggFileUploadController', ['$scope','$uibModalInstance','mission','Upload','$window', function($scope,$uibModalInstance,mission,Upload,$window) {
+sourceApp.controller('aggFileUploadController', ['$scope','$uibModalInstance','mission','Upload','$window','configService','sourceip', function($scope,$uibModalInstance,mission,Upload,$window,configService,sourceip) {
 
     var $ctrl = this;
     $ctrl.missionName = mission;
@@ -76,18 +79,8 @@ sourceApp.controller('aggFileUploadController', ['$scope','$uibModalInstance','m
         file : "",
         id: ""
     }
-
-    $ctrl.list = [
-        {
-            id:100,
-            filename:'AggFile1.csv'
-        },
-        {
-            id:101,
-            filename:'AggFile2.csv'
-        }
-    ]
-
+    $ctrl.sourceip = sourceip;
+    showAttachmentsForConfig(sourceip);
     $ctrl.close = function() {
         $uibModalInstance.dismiss('cancel');
     };
@@ -96,27 +89,109 @@ sourceApp.controller('aggFileUploadController', ['$scope','$uibModalInstance','m
         // Call upload if form is valid
         if($ctrl.uploadAggFile_form.$valid) {
             if($ctrl.formDetails.file){
-                $ctrl.upload($ctrl.formDetails.file); 
+                //get all attachments
+                //check for existing id
+                //if id already exists ask user if he wants to update
+                //if yes upload the file
+                //else dont upload
+                configService.getAttachments(sourceip).then(function(response){
+                    if(response.status === 200){
+                        $ctrl.count = 0;
+                        if(response.data.length > 0){
+                            for(var i=0;i<response.data.length;i++){
+                                if(parseInt(response.data[i].id) === $ctrl.formDetails.id){
+                                    if($window.confirm("This id already exists in the database.Do you want to update the file?")){
+                                        $ctrl.count = 0;
+                                        break;
+                                    }else {
+                                        $ctrl.count = $ctrl.count + 1;
+                                        $ctrl.formDetails = {};
+                                        $ctrl.uploadAggFile_form.$setPristine();
+                                        break;
+                                    }
+                                }
+                            }
+                        }else {
+                            $ctrl.count = 0;
+                        }
+
+                        if($ctrl.count === 0){
+                            $ctrl.uploadCsv($ctrl.formDetails.file); 
+                        }
+                    }
+                });
             } else {
                 $window.alert('No file passed. Please upload a csv file.');
             }
         }
     }
 
-    $ctrl.upload = function(file) {
+    $ctrl.uploadCsv = function(file) {
         Upload.upload({
-            url: '/uploadCsv', 
+            url: '/saveAggregatorFile', 
             data: { 
                 file : file, 
                 id : $ctrl.formDetails.id, 
-                mission : $ctrl.missionName
+                sourceip : sourceip
             } 
         }).then(function (resp) { 
             //validate success
+            if(resp.data.error_code === 0 && resp.data.error_desc === "add"){ 
+                showAttachmentsForConfig(sourceip);
+                $window.alert('Success: '+resp.config.data.file.name+" uploaded.");
+
+                //reset the input fields on the form
+                $ctrl.formDetails = {};
+                $ctrl.uploadAggFile_form.$setPristine();
+            } else if(resp.data.error_code === 0 && resp.data.error_desc === "update"){
+                showAttachmentsForConfig(sourceip);
+                $window.alert('Success: '+resp.config.data.file.name+" updated.");
+
+                //reset the input fields on the form
+                $ctrl.formDetails = {};
+                $ctrl.uploadAggFile_form.$setPristine();
+            }else if(resp.data.error_code === 1 && resp.data.error_desc === "Does not have all the rows data"){
+                $window.alert("All the rows in the file should have both parameter value and bit length value.Please recheck your file.");
+                //reset the input fields on the form
+                $ctrl.formDetails = {};
+                $ctrl.uploadAggFile_form.$setPristine();
+            }else {
+                $window.alert('An error occured');
+            }
 
         }, function (resp) { //catch error
-            
+            $window.alert('Error status: ' + resp.status);
         });
     };
+
+    function showAttachmentsForConfig(sourceip){
+        configService.getAttachments(sourceip).then(function(response){
+            if(response.status === 200){
+                $ctrl.list = [];
+                if(response.data.length > 0){
+                    for(var i=0;i<response.data.length;i++){
+                        $ctrl.list.push({
+                            'id':response.data[i].id,
+                            'filename':response.data[i].filename
+                        });
+                    }
+                }else {
+                    $ctrl.list = [];
+                }
+            }
+        });
+    }
+
+    $ctrl.removeAttachment = function(id,sourceip){
+        if($window.confirm('Are you sure you want to delete this attachment?')){
+            configService.removeAttachment(id,sourceip)
+            .then(function(response) {
+                if(response.data.error_code === 0) {
+                    showAttachmentsForConfig(sourceip);
+                    $window.alert('Aggregator File with Id: ' + response.config.data.id +' is deleted.');
+                }
+            })
+        }
+    }
 
 }]);
