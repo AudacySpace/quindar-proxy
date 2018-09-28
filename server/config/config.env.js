1b61b020a02f181f297acde384a841e682269335
Chavi Malhotra <chavi.malhotra@gmail.com>
2018-09-28 10:59:03 -0700

    implement encrypting of keys

--- server/config/config.env.js
+++ server/config/config.env.js
@@ -1,18 +1,36 @@
+// Grab the config env file if it's there
+var config;
+
+try{
+    config = require('./config.env');
+} catch(e) {
+    config = {};
+}
+
 module.exports = function(){
-    switch(process.env.NODE_ENV){
-        case 'staging':
+    if(isEmpty(config)){
+        //default values if configuration not present
+        return {
+            'databaseURL' : 'mongodb://localhost:27017/quindar'
+        };
+    } else {
+        //values of NODE_ENV - 'staging', 'production', 'development'
+        if(process.env.NODE_ENV) {
             return {
-				'databaseURL' : 'mongodb://54.184.232.90:27017/quindar'
+                'databaseURL' : config[process.env.NODE_ENV].databaseURL
             };
-
-        case 'production':
+        } else { //return values for development environment in case NODE_ENV not set
             return {
-				'databaseURL' : ''
+                'databaseURL' : config.development.databaseURL
             };
-
-        default:
-            return {
-				'databaseURL' : 'mongodb://54.184.232.90:27017/quindar'
-            };
+        }
     }
 };
+
+function isEmpty(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
